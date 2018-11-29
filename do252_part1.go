d68497da138db767414a71c804a13e122a630b9c
David Omenyo <davidomenyo@gmail.com>
2018-11-29 15:14:37 +0000

    first commit

---
+++ do252_part1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World!")
+}
+
+func lecturer(wait chan chan string, lect chan bool){
+
+}
+
+func student(wait chan chan string, lect chan bool, name string){
+
+}
